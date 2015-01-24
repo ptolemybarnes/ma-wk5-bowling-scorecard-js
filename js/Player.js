10efbce9e4ba80fe31515c5ef85dbd2de32a259b
Ptolemy <ptolemybarnes@gmail.com>
2015-01-24 13:56:36 +0000

    Player can be in bonus mode and receive double points.

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,20 @@
 var Player = function() {
-  this.score = 0;
+  this.score       = 0;
+  this.bonusStreak = 0; // tracks remaining rounds wherein player can score bonus points.
+};
+
+Player.prototype.receiveScore = function(points) {
+  if (this.bonusStreak > 0) {
+    var points = (points * 2);
+    this.bonusStreak -= 1;
+  };
+  this.increaseScore(points);
+};
+
+Player.prototype.increaseScore = function(points) {
+  this.score += points;
+};
+
+Player.prototype.startBonusStreak = function() {
+  this.bonusStreak = 2;
 };
--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -13,4 +13,31 @@ describe('Player', function() {
       expect(player.score).toEqual(0);
     });
   });
+
+  describe('when not in bonus mode', function() {
+    it('score can be increased', function() {
+      player.receiveScore(5);
+
+      expect(player.score).toEqual(5);
+    });
+  });
+
+  describe('when in bonus mode', function() {
+
+    it('score increases by double', function() {
+      player.startBonusStreak();
+      player.receiveScore(5);
+
+      expect(player.score).toEqual(10);
+    });
+
+    it('score increases by double twice', function() {
+      player.startBonusStreak();
+
+      player.receiveScore(5);
+      player.receiveScore(7);
+
+      expect(player.score).toEqual(24);
+    });
+  });
 });
