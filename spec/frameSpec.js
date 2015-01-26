b9d3127b6e5e13f1a538312736d209d59415127e
Ptolemy <ptolemybarnes@gmail.com>
2015-01-26 12:04:21 +0000

    Create TenthFrame, child class of Frame.

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -22,3 +22,43 @@ describe('Frame', function() {
     expect( function(){ frame.knockDown(2) }).toThrowError('The frame is over! No more rolls.')
   });
 });
+
+describe('TenthFrame', function() {
+  var tenthFrame;
+
+  it('knows frame is not over after a strike', function() {
+    tenthFrame = new TenthFrame(3);
+
+    tenthFrame.knockDown(10);
+
+    expect(tenthFrame.isFrameOver()).toBe(false);
+  });
+
+  it('knows frame is not over after a spare', function() {
+    tenthFrame = new TenthFrame(3);
+
+    tenthFrame.knockDown(5);
+    tenthFrame.knockDown(5);
+
+    expect(tenthFrame.isFrameOver()).toBe(false);
+  });
+
+  it('knows frame is over if not a strike or a spare', function() {
+    tenthFrame = new TenthFrame(3);
+
+    tenthFrame.knockDown(5);
+    tenthFrame.knockDown(3);
+
+    expect(tenthFrame.isFrameOver()).toBe(true);
+  });
+
+  it('knows frame is over after three rolls', function() {
+    tenthFrame = new TenthFrame(3);
+
+    tenthFrame.knockDown(10);
+    tenthFrame.knockDown(10);
+    tenthFrame.knockDown(10);
+
+    expect(tenthFrame.isFrameOver()).toBe(true);
+  });
+});
